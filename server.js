6b1df7b2f9b0deafb88c4b94610672dc874f17e1
RanaUsman5421 <153462482+RanaUsman5421@users.noreply.github.com>
2026-02-15 20:09:07 +0500

    Change nodemailer auth to use hardcoded credentials
    Updated email credentials for nodemailer transport.

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.get('/api/data', (req, res) => {
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'macktech28@gmail.com',
+    pass: ''kcwmqkdjbyryonbt,
   }
 })
 
